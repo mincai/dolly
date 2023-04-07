5caaef8dce102efb5cd127dc688ad155f04637b2
lbwavebo2022 <bo.ling@uber.com>
2023-04-07 21:20:21 +0000

    modification

--- agent_train.sh
+++ agent_train.sh
@@ -6,8 +6,8 @@ deepspeed \
      --module training.agent_trainer \
      --deepspeed config/ds_z3_bf16_config.json \
      --epochs 1 \
-     --local-output-dir /home/bo_ling/dolly_training/agent_modeling_data_v1 \
-     --local-data-file-path /home/bo_ling/dataset/modeling_data_v1.hf \
+     --local-output-dir /home/bo_ling/dolly_training/agent_modeling_data_v1_expanded \
+     --local-data-file-path /home/bo_ling/dataset/modeling_data_v1_expanded.hf \
      --per-device-train-batch-size 1 \
      --per-device-eval-batch-size 1 \
      --test-size 100 \
