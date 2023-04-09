ce718ddfdfc5d21ff9fa06c55e4c7425c8f071b7
lbwavebo2022 <bo.ling@uber.com>
2023-04-09 01:50:19 +0000

    bug fixing

--- transcript_train.sh
+++ transcript_train.sh
@@ -6,8 +6,8 @@ deepspeed \
      --module training.transcript_trainer \
      --deepspeed config/ds_z3_bf16_config.json \
      --epochs 1 \
-     --local-output-dir /home/bo_ling/dolly_training/doc_transcript_pii_data_simplify_b1 \
-     --local-data-file-path /home/bo_ling/dataset/doc_transcript_pii_data_simplify.hf \
+     --local-output-dir /home/bo_ling/dolly_training/doc_transcript_pii_data_corrected \
+     --local-data-file-path /home/bo_ling/dataset/doc_transcript_pii_data_corrected.hf \
      --per-device-train-batch-size 1 \
      --per-device-eval-batch-size 1 \
      --test-size 100 \
