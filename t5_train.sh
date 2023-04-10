c8eff7e537e1b1b2480523a33b2beb6032e3d608
lbwavebo2022 <bo.ling@uber.com>
2023-04-10 03:05:31 +0000

    add t5 trainer

---
+++ t5_train.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+deepspeed \
+     --include="localhost:0,1" \
+     --master_port 29500 \
+     --module training.trainer \
+     --deepspeed config/ds_z3_bf16_config.json \
+     --epochs 5 \
+     --local-output-dir /home/bo_ling/dolly_training/t5_doc_transcript \
+     --local-data-file-path /home/bo_ling/dataset/doc_transcript_pii_data_t5.hf \
+     --per-device-train-batch-size 2 \
+     --per-device-eval-batch-size 2 \
+     --test-size 100 \
+     --lr 1e-5
