4e7e4836b12ecc0423f66753066e6ba0c127509a
lbwavebo2022 <bo.ling@uber.com>
2023-04-18 16:54:23 +0000

    bug fixing

--- t5_train.sh
+++ t5_train.sh
@@ -6,8 +6,8 @@ deepspeed \
      --module training.t5_trainer \
      --deepspeed config/ds_z3_bf16_config.json \
      --epochs 10 \
-     --local-output-dir /home/bo_ling/dolly_training/eats_receipt_gcp_t5_new \
-     --local-data-file-path /home/bo_ling/dataset/eats_receipt_gcp_train.hf \
+     --local-output-dir /home/bo_ling/dolly_training/eats_receipt_gcp_t5_v3 \
+     --local-data-file-path /home/bo_ling/dataset/eats_receipt_gcp_v3_train.hf \
      --per-device-train-batch-size 8 \
      --per-device-eval-batch-size 8 \
      --test-size 10 \
